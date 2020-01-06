28badf09720a6a1ae88bba597a7c8a5c227266ae
Ronodeep Bhattacharya <ronodeep_b@persistent.co.in>
2020-01-06 11:32:34 +0530

    pagination implemented for messages resource

--- JAVARestApi/src/main/java/com/spideron/rest/resource/MessageResource.java
+++ JAVARestApi/src/main/java/com/spideron/rest/resource/MessageResource.java
@@ -10,6 +10,7 @@ import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
 import com.spideron.model.Message;
@@ -22,9 +23,26 @@ public class MessageResource {
 	
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
-	public List<Message> getAllMessages() {
-		
-		return mesServ.getAllMessages();
+	public List<Message> getAllMessages(@QueryParam("year")int year,@QueryParam("start")int start,@QueryParam("size")int size) {
+		System.out.println("Year="+year);
+		System.out.println("Start="+start);
+		System.out.println("Size="+size);
+		if(year>0 && start>=0 && size>0) {
+			System.out.println("Case 1");
+			return mesServ.getMessagesOnPagination(year, start, size);
+		}
+		else if(year<=0 && start>=0 && size>0) {
+			System.out.println("Case 2");
+			return mesServ.getMessagesOnPagination(year, start, size);
+		}
+		else if(year>0) {
+			System.out.println("Case 3");
+			return mesServ.getMessagesForYear(year);
+		}
+		else {
+			System.out.println("Case 4");
+			return mesServ.getAllMessages();
+		}
 	}
 	
 	@POST
--- JAVARestApi/src/main/java/com/spideron/service/MessageService.java
+++ JAVARestApi/src/main/java/com/spideron/service/MessageService.java
@@ -1,6 +1,7 @@
 package com.spideron.service;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
@@ -28,7 +29,37 @@ public class MessageService {
 
 		return messageMap.get(messageid);
 	}
-
+	
+	public List<Message> getMessagesForYear(int year) {
+		Calendar calendar=Calendar.getInstance();
+		
+		List<Message> messsageList = new ArrayList<Message>();
+		for (Message message : messageMap.values()) {
+			calendar.setTime(message.getCreated());
+			if(year==calendar.get(Calendar.YEAR)) {
+				messsageList.add(message);
+			}
+		}
+		return messsageList;
+	}
+	
+	public List<Message> getMessagesOnPagination(int year,int start, int size) {
+		Calendar calendar=Calendar.getInstance();
+		
+		List<Message> messsageList = new ArrayList<Message>();
+		if(year<=0) {
+			messsageList = new ArrayList<Message>(messageMap.values());
+		}
+		else {
+			for (Message message : messageMap.values()) {
+				calendar.setTime(message.getCreated());
+				if(year==calendar.get(Calendar.YEAR)) {
+					messsageList.add(message);
+				}
+			}
+		}
+		return messsageList.subList(start, start+size);
+	}
 	public Message addMessage(Message message) {
 
 		message.setId(messageMap.size() + 1);
